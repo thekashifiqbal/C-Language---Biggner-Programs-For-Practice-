5559538da081e6248c42259b39aa4d8c2175fc46
Kashif iQbal <112928318+thekashifiqbal@users.noreply.github.com>
2025-11-28 22:20:55 +0500

    Add files via upload

---
+++ 18_Memory_Adresses.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int age = 30;
+    double gpa = 3.8;
+    char grade = 'A';
+    printf("Age : %p\nGpa : %p\nGrade : %p\n",&age, &gpa, &grade);
+    
+    return 0;
+}

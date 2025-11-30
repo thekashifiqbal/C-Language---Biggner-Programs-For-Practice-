b681b9639f45c0756845a71f755f37b6227c2a8f
Kashif iQbal <112928318+thekashifiqbal@users.noreply.github.com>
2025-11-30 23:11:51 +0500

    Add files via upload

---
+++ 19_Pointers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int age = 30;
+    int * pAge = &age;
+    double gpa = 3.8;
+    double * pGpa = &gpa;
+    char grade = 'A';
+    char * pGrade = &grade;
+    
+    return 0;
+}
